8fb3af180d28426cec9db78ee6dfcc27528ba22a
Julsperez <julsperez@outlook.com>
2020-05-10 21:11:45 -0500

    before erase treenode server practica14/optimizacion

--- practica14/optimizacion/Servidor.cpp
+++ practica14/optimizacion/Servidor.cpp
@@ -8,56 +8,38 @@
 
 using namespace std;
 
-//////////////////////////////////////////////////////////////////////////////
 const int ALPHABET_SIZE = 11; 
 
 // trie node 
-struct TrieNode 
-{ 
+struct TrieNode { 
     struct TrieNode *children[ALPHABET_SIZE]; 
-  
-    // isEndOfWord is true if the node represents 
-    // end of a word 
     bool isEndOfWord; 
 }; 
   
 // Returns new trie node (initialized to NULLs) 
-struct TrieNode *getNode(void) 
-{ 
+struct TrieNode *getNode(void) { 
     struct TrieNode *pNode =  new TrieNode; 
-  
     pNode->isEndOfWord = false; 
-  
     for (int i = 0; i < ALPHABET_SIZE; i++) 
         pNode->children[i] = NULL; 
-  
     return pNode; 
 } 
   
-// If not present, inserts key into trie 
-// If the key is prefix of trie node, just 
-// marks leaf node 
-void insert(struct TrieNode *root, string key) 
-{ 
+
+void insert(struct TrieNode *root, string key) { 
     struct TrieNode *pCrawl = root; 
   
-    for (int i = 0; i < key.length(); i++) 
-    { 
+    for (int i = 0; i < key.length(); i++) { 
         int index = key[i] - 'a'; 
         if (!pCrawl->children[index]) 
             pCrawl->children[index] = getNode(); 
-  
         pCrawl = pCrawl->children[index]; 
     } 
-  
-    // mark last node as leaf 
+
     pCrawl->isEndOfWord = true; 
 } 
-  
-// Returns true if key presents in trie, else 
-// false 
-bool search(struct TrieNode *root, string key) 
-{ 
+ 
+bool search(struct TrieNode *root, string key) { 
     struct TrieNode *pCrawl = root; 
   
     for (int i = 0; i < key.length(); i++) 
@@ -70,8 +52,7 @@ bool search(struct TrieNode *root, string key)
     } 
   
     return (pCrawl != NULL && pCrawl->isEndOfWord); 
-} 
-//////////////////////////////////////////////////////////////////////////////
+}
 
 struct timeval timeNow;
 
@@ -82,10 +63,8 @@ struct timeval getTimeOfDay(){
 
 int main(int argc, char const *argv[]) {
 	long int idEsperado =0, idAnterior = -1;
-	/////////////////////////////////////////////
 	struct TrieNode *root = getNode();
 	int flag=0;
-	/////////////////////////////////////////////
 	struct timeval timeout;
   	timeout.tv_sec = 100;
   	timeout.tv_usec = 500000;
